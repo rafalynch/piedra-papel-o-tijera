8def435a85e4b925e52bf07ca2760c5f82e50680
Rafa Lynch <rafa_lynch@hotmail.com>
2021-07-29 20:28:00 -0300

    switch

--- src/components/result/index.ts
+++ src/components/result/index.ts
@@ -15,23 +15,25 @@ customElements.define(
       const style = document.createElement("style");
       div.className = "result-title";
 
-      if (this.getAttribute("label") == "player") {
-        div.innerHTML = `
+      switch (this.getAttribute("label")) {
+        case "player":
+          div.innerHTML = `
           <h2>Ganaste!</h2>
         `;
-        div.style.backgroundColor = "green";
-      }
-      if (this.getAttribute("label") == "tie") {
-        div.innerHTML = `
+          div.style.backgroundColor = "green";
+          break;
+        case "tie":
+          div.innerHTML = `
           <h2>Empate!</h2>
         `;
-        div.style.backgroundColor = "lightblue";
-      }
-      if (this.getAttribute("label") == "computer") {
-        div.innerHTML = `
+          div.style.backgroundColor = "lightblue";
+          break;
+        case "computer":
+          div.innerHTML = `
         <h2>Perdiste!</h2>
         `;
-        div.style.backgroundColor = "red";
+          div.style.backgroundColor = "red";
+          break;
       }
 
       style.innerHTML = `
